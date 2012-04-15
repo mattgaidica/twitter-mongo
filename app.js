4287701d3042bda69768dab4488c060e5ffa9c56
Matt <matt@primestudiosllc.com>
2012-04-15 01:11:57 -0700

    init

---
+++ app.js
@@ -0,0 +1,102 @@
+var express = require('express')
+  , mongoose = require('mongoose')
+  , passport = require('passport')
+  , util = require('util')
+  , TwitterStrategy = require('passport-twitter').Strategy
+  , Schema = mongoose.Schema;
+
+var TWITTER_CONSUMER_KEY = "whyh7fXj3FgTJ5IEmHVg"
+var TWITTER_CONSUMER_SECRET = "a7Z7X9HarAwo4rDniJAZdWJtwduMPazpNqkIVkEjaM";
+
+var UserSchema = new Schema({
+  provider: String,
+  uid: String,
+  name: String,
+  image: String,
+  created: {type: Date, default: Date.now}
+});
+
+mongoose.connect('mongodb://localhost/twitter-mongo');
+mongoose.model('User', UserSchema);
+
+var User = mongoose.model('User');
+
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOne({uid: profile.id}, function(err, user) {
+      if(user) {
+        done(null, user);
+      } else {
+        var user = new User();
+        user.provider = "twitter";
+        user.uid = profile.id;
+        user.name = profile.displayName;
+        user.image = profile._json.profile_image_url;
+        user.save(function(err) {
+          if(err) { throw err; }
+          done(null, user);
+        });
+      }
+    })
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.uid);
+});
+
+passport.deserializeUser(function(uid, done) {
+  User.findOne({uid: uid}, function (err, user) {
+    done(err, user);
+  });
+});
+
+var app = express.createServer();
+
+// configure Express
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/auth/twitter',
+  passport.authenticate('twitter'),
+  function(req, res){
+    // The request will be redirected to Twitter for authentication, so this
+    // function will not be called.
+  });
+
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(3000);
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
